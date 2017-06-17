32eea0c83fea2403980720da05f0553703fc7534
Michał Oleszczuk <moleszczuk@teonite.com>
2017-06-17 11:21:14 +0200

    HTML base

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import test_view
 
 urlpatterns = [
-    url(r'^test/', test_view)
+    url(r'^', test_view)
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,9 +1,12 @@
 import logging
 from django.shortcuts import render
+from apps.core.models import Person
+from config.settings import BASE_DIR
 
 log = logging.getLogger(__name__)
 
 
 def test_view(request):
+    contacts = Person.objects.all().order_by('name')
     log.info('view visited')
-    return render(request, 'test.html')
+    return render(request, 'contacts_list.html', {'contacts': contacts, 'BASE_DIR': BASE_DIR})
